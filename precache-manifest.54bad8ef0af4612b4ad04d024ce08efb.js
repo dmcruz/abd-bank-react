34b5bfe9fea19c5cfef71df1fdcde1d9e8b053e9
Donna <dmcruz@gmail.com>
2020-10-03 14:27:37 +0800

    Updates

---
+++ precache-manifest.54bad8ef0af4612b4ad04d024ce08efb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "665c0057b0dc98c6f1311138a59409cd",
+    "url": "/abd-bank-react/index.html"
+  },
+  {
+    "revision": "97fc3dd9a3cd94a6f439",
+    "url": "/abd-bank-react/static/css/2.f0033150.chunk.css"
+  },
+  {
+    "revision": "c800d7ad945326fbf576",
+    "url": "/abd-bank-react/static/css/main.c3dc9c83.chunk.css"
+  },
+  {
+    "revision": "97fc3dd9a3cd94a6f439",
+    "url": "/abd-bank-react/static/js/2.31a50330.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/abd-bank-react/static/js/2.31a50330.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c800d7ad945326fbf576",
+    "url": "/abd-bank-react/static/js/main.2e9253ed.chunk.js"
+  },
+  {
+    "revision": "59f854808780ba08a908",
+    "url": "/abd-bank-react/static/js/runtime-main.4c519cf6.js"
+  }
+]);
