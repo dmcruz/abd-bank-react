41fd0c56c71b432efc068baf7154e11d90675aae
Donna <dmcruz@gmail.com>
2020-10-07 23:54:09 +0800

    add paging to Critters

--- src/pages/critters/Bugs.js
+++ src/pages/critters/Bugs.js
@@ -1,125 +1,13 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { addItemToBag } from '../../redux/user/user.action';
-import { message, Button, Alert, Row, Col, Avatar} from 'antd';
+import Critters from './Critters';
 
 class Bugs extends Component {
 
-  constructor() {
-    super();
-    this.state = {
-        error: null,
-        bugs: [],
-        loading: true   
-    }
-  }
-
-  componentDidMount() {
-
-    this.loadBugs();
-  }
-
-  renderList(bugs) {
-      return (
-        <div>
-            <Row gutter={16} justify="start" style={{ textAlign: 'left'}}>
-                <Col span={6}>
-                    Name
-                </Col>
-                <Col span={4}>
-                    Price
-                </Col>
-                <Col span={6}>
-                    Image
-                </Col>
-                <Col span={6}>
-                    Action
-                </Col>
-            </Row>
-
-            {bugs.map(bug =>
-                <Row gutter={16} justify="start" style={{ textAlign: 'left'}} key={bug.id}>
-                    <Col span={6}>
-                        {bug.name["name-USen"]}
-                    </Col>
-                    <Col span={4}>
-                        {bug["price"]}
-                    </Col>
-                    <Col span={6}>
-                        <Avatar src={bug["image_uri"]} />
-                    </Col>
-                    <Col span={6}>
-                        <Button type="primary" value="1" onClick={()=> this.onAdd(bug)}>Catch</Button>
-                    </Col>
-                </Row>
-            )
-            }
-        </div>
-      );
-  }
-
-  onAdd(item) {
-
-    var obj = {
-        id: "bug-" + item.id,
-        name: item.name["name-USen"],
-        price: item.price,
-        imageUri: item["image_uri"]
-    };
-    this.props.addItemToBag(obj);
-
-    message.success("Added " + obj.name + "!");
-  }
-
   render () {
-      let errorAlert=<span/>;
-      if (this.state.error && this.state.error !== '') {
-          errorAlert = <Alert type="error">this.state.error</Alert>
-      }
-
-    let contents = this.state.loading
-    ? <p><em>Loading...</em></p>
-    : this.renderList(this.state.bugs);
       
     return (
-      <div align="center">
-            {errorAlert}
-            {contents}
-      </div>
+      <Critters type="bug" apiUrl="https://acnhapi.com/v1a/bugs" />
     );
   }
-  
-  loadBugs() {
-
-    var me = this;
-
-    fetch('https://acnhapi.com/v1a/bugs')
-    .then(
-        function(response) {
-        if (response.status !== 200) {
-
-            me.setState({ error: 'Something went wrong. Status code: ' + response.status});
-            console.log('Looks like there was a problem. Status Code: ' +
-            response.status);
-            return;
-        }
-
-        response.json().then(function(data) {
-            me.setState({ bugs: data, loading: false, error: null});
-        });
-        }
-    )
-    .catch(function(err) {
-        console.log('Fetch Error :-S', err);
-        me.setState({ error: 'Fetch error'});
-    });
-  }
-  
 }
-
-
-const mapStateToProps = state => ({
-    inventory: state.user.inventory
-  });
-
-export default connect(mapStateToProps, {addItemToBag})(Bugs);
+export default Bugs;
---
+++ src/pages/critters/Critters.js
@@ -0,0 +1,154 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addItemToBag } from '../../redux/user/user.action';
+import { message, Button, Alert, Row, Col, Image, Pagination} from 'antd';
+
+class Critters extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+        error: null,
+        critters: [],
+        loading: true,
+        currentPage: 0,
+        defaultPageSize: 5,
+        pageSize: 5,
+        totalRecords: 0   
+    }
+    this.onShowSizeChange = this.onShowSizeChange.bind(this);
+    this.onPageChange = this.onPageChange.bind(this);
+  }
+
+  componentDidMount() {
+
+    this.loadCritters();
+  }
+
+  renderList(critters) {
+      return (
+        <div style={{margin: 'auto', width: '95%'}}>
+            <Row gutter={16} justify="start" style={{ textAlign: 'center', fontWeight: 'bold'}}>
+                <Col span={10} style={{ textAlign: 'left'}}>
+                    Name
+                </Col>
+                <Col span={4}>
+                    Price
+                </Col>
+                <Col span={6}>
+                    Image
+                </Col>
+                <Col span={2}>
+                    Action
+                </Col>
+            </Row>
+
+            {critters.map(critter =>
+                <Row gutter={16} justify="start" style={{ textAlign: 'center', padding: '25px 0', backgroundColor: critter.id % 2 !== 0 ? '#eee' : 'transparent'}} key={critter.id}>
+                    <Col span={10} style={{ textAlign: 'left' }}>
+                        {critter.name["name-USen"]}
+                    </Col>
+                    <Col span={4}>
+                        {critter["price"]}
+                    </Col>
+                    <Col span={6}>
+                        <Image src={critter["image_uri"]} width={80}/>
+                    </Col>
+                    <Col span={2}>
+                        <Button type="primary" value="1" onClick={()=> this.onAdd(critter)}>Catch</Button>
+                    </Col>
+                </Row>
+            )
+            }
+
+            <Pagination showQuickJumper defaultCurrent={this.state.currentPage} 
+                defaultPageSize={this.state.defaultPageSize} 
+                pageSize={this.state.pageSize}
+                total={this.state.totalRecords} 
+                onShowSizeChange={this.onShowSizeChange}
+                onChange={this.onPageChange}
+                />
+        </div>
+      );
+  }
+
+  onShowSizeChange(current, pageSize) {
+    this.setState({pageSize: pageSize});
+    this.paginate(this.state.critters);
+  }
+
+  onPageChange(page, pageSize) {
+    this.setState({currentPage: page, pageSize: pageSize});
+    this.paginate(this.state.critters);
+  }
+
+  paginate(array) {
+    return array.slice((this.state.currentPage - 1) * this.state.pageSize, this.state.currentPage * this.state.pageSize);
+  }
+
+  onAdd(item) {
+
+    var obj = {
+        id: this.props.type + "-" + item.id,
+        name: item.name["name-USen"],
+        price: item.price,
+        imageUri: item["image_uri"]
+    };
+    this.props.addItemToBag(obj);
+
+    message.success("Added " + obj.name + "!");
+  }
+
+  render () {
+      let errorAlert=<span/>;
+      if (this.state.error && this.state.error !== '') {
+          errorAlert = <Alert type="error">this.state.error</Alert>
+      }
+
+    let contents = this.state.loading
+    ? <p><em>Loading...</em></p>
+    : this.renderList(this.paginate(this.state.critters));
+      
+    return (
+      <div align="center">
+            {errorAlert}
+            {contents}
+      </div>
+    );
+  }
+  
+  loadCritters() {
+    var me = this;
+
+    fetch(this.props.apiUrl)
+    .then(
+        function(response) {
+        if (response.status !== 200) {
+
+            me.setState({ error: 'Something went wrong. Status code: ' + response.status});
+            console.log('Looks like there was a problem. Status Code: ' +
+            response.status);
+            return;
+        }
+
+        response.json().then(function(data) {
+            me.setState({ critters: data, loading: false, 
+                error: null, 
+                currentPage: data && data.length > 0 ? 1 : 0, 
+                totalRecords: data.length});
+        });
+        }
+    )
+    .catch(function(err) {
+        console.log('Fetch Error :-S', err);
+        me.setState({ error: 'Fetch error'});
+    });
+  }
+}
+
+
+const mapStateToProps = state => ({
+    inventory: state.user.inventory
+  });
+
+export default connect(mapStateToProps, {addItemToBag})(Critters);
